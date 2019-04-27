53fb4242abf187f3ee898c919f327f8507276327
AlexECX <33321263+AlexECX@users.noreply.github.com>
2019-04-27 16:46:58 -0400

    MusicLibraryLoader peut load un download

--- Application/src/main/java/com/example/android/mediasession/service/contentcatalogs/MusicLibraryLoader.java
+++ Application/src/main/java/com/example/android/mediasession/service/contentcatalogs/MusicLibraryLoader.java
@@ -1,22 +1,19 @@
 package com.example.android.mediasession.service.contentcatalogs;
 
+import android.content.Context;
 import android.database.Cursor;
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
 import android.util.Log;
 
+import java.io.File;
+import java.io.FileDescriptor;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
+import android.media.MediaScannerConnection;
 
-import static com.example.android.mediasession.service.contentcatalogs.MusicDatabase.KEY_ALBUM;
-import static com.example.android.mediasession.service.contentcatalogs.MusicDatabase.KEY_ALBUM_ART_RES_ID;
-import static com.example.android.mediasession.service.contentcatalogs.MusicDatabase.KEY_ALBUM_ART_RES_NAME;
-import static com.example.android.mediasession.service.contentcatalogs.MusicDatabase.KEY_ARTIST;
-import static com.example.android.mediasession.service.contentcatalogs.MusicDatabase.KEY_DURATION;
-import static com.example.android.mediasession.service.contentcatalogs.MusicDatabase.KEY_DURATION_UNIT;
-import static com.example.android.mediasession.service.contentcatalogs.MusicDatabase.KEY_GENRE;
-import static com.example.android.mediasession.service.contentcatalogs.MusicDatabase.KEY_MUSIC_FILENAME;
-import static com.example.android.mediasession.service.contentcatalogs.MusicDatabase.KEY_TITRE;
-import static com.example.android.mediasession.service.contentcatalogs.MusicDatabase.KEY_URI_STRING;
+import com.example.android.R;
 
 public class MusicLibraryLoader {
 
@@ -46,18 +43,90 @@ public class MusicLibraryLoader {
     public static void loadFromDB(MusicDatabase dbMusic) {
         for (HashMap<String,String>chanson : dbMusic.getTrackList()) {
             MusicLibrary.createMediaMetadataCompat(
-                    chanson.get(KEY_URI_STRING),
-                    chanson.get(KEY_TITRE),
-                    chanson.get(KEY_ARTIST),
-                    chanson.get(KEY_ALBUM),
-                    chanson.get(KEY_GENRE),
-                    Long.valueOf(chanson.get(KEY_DURATION)),
-                    TimeUnit.valueOf(chanson.get(KEY_DURATION_UNIT)),
-                    chanson.get(KEY_MUSIC_FILENAME),
-                    Integer.valueOf(chanson.get(KEY_ALBUM_ART_RES_ID)),
-                    chanson.get(KEY_ALBUM_ART_RES_NAME)
+                    chanson.get(MusicDatabase.KEY_URI_STRING),
+                    chanson.get(MusicDatabase.KEY_TITRE),
+                    chanson.get(MusicDatabase.KEY_ARTIST),
+                    chanson.get(MusicDatabase.KEY_ALBUM),
+                    chanson.get(MusicDatabase.KEY_GENRE),
+                    Long.valueOf(chanson.get(MusicDatabase.KEY_DURATION)),
+                    TimeUnit.valueOf(chanson.get(MusicDatabase.KEY_DURATION_UNIT)),
+                    chanson.get(MusicDatabase.KEY_MUSIC_FILENAME),
+                    Integer.valueOf(chanson.get(MusicDatabase.KEY_ALBUM_ART_RES_ID)),
+                    chanson.get(MusicDatabase.KEY_ALBUM_ART_RES_NAME)
             );
         }
+    }
+
+    public static void loadFromNewFile(Context context, File file) {
+        MediaScannerConnection.scanFile(
+                context,
+                new String[]{file.getAbsolutePath()},
+                null,
+                new MediaScannerConnection.OnScanCompletedListener() {
+                    @Override
+                    public void onScanCompleted(String path, Uri uri) {
+                        MediaMetadataRetriever mmr = new MediaMetadataRetriever();
+
+                        mmr.setDataSource(path);
+                        MusicLibrary.createMediaMetadataCompat(
+                                uri.toString(),
+                                mmr.extractMetadata(mmr.METADATA_KEY_TITLE),
+                                mmr.extractMetadata(mmr.METADATA_KEY_ARTIST),
+                                mmr.extractMetadata(mmr.METADATA_KEY_ALBUM),
+                                mmr.extractMetadata(mmr.METADATA_KEY_GENRE),
+                                TimeUnit.MILLISECONDS.toSeconds(
+                                        Long.parseLong(
+                                                mmr.extractMetadata(mmr.METADATA_KEY_DURATION)
+                                        )
+                                ),
+                                TimeUnit.SECONDS,
+                                path,
+                                R.drawable.album_jazz_blues,
+                                "album_jazz_blues"
+                        );
+                        Log.v("MUSICLIBRARYLOADER",
+                                "file " + path + " was scanned seccessfully: " + uri);
+                    }
+                });
+
+
+    }
+
+    public static void loadFromNewFile(Context context, ArrayList<File> files) {
+        ArrayList<String> paths = new ArrayList<>();
+        for (File file : files) {
+            paths.add(file.getAbsolutePath());
+        }
+        MediaScannerConnection.scanFile(
+                context,
+                paths.toArray(new String[0]),
+                null,
+                new MediaScannerConnection.OnScanCompletedListener() {
+                    @Override
+                    public void onScanCompleted(String path, Uri uri) {
+                        MediaMetadataRetriever mmr = new MediaMetadataRetriever();
+
+                        mmr.setDataSource(path);
+                        MusicLibrary.createMediaMetadataCompat(
+                                uri.toString(),
+                                mmr.extractMetadata(mmr.METADATA_KEY_TITLE),
+                                mmr.extractMetadata(mmr.METADATA_KEY_ARTIST),
+                                mmr.extractMetadata(mmr.METADATA_KEY_ALBUM),
+                                mmr.extractMetadata(mmr.METADATA_KEY_GENRE),
+                                TimeUnit.MILLISECONDS.toSeconds(
+                                        Long.parseLong(
+                                                mmr.extractMetadata(mmr.METADATA_KEY_DURATION)
+                                        )
+                                ),
+                                TimeUnit.SECONDS,
+                                path,
+                                R.drawable.album_jazz_blues,
+                                "album_jazz_blues"
+                        );
+                        Log.v("MUSICLIBRARYLOADER",
+                                "file " + path + " was scanned seccessfully: " + uri);
+                    }
+                });
     }
 
 }
